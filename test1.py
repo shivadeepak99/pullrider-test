e74bcd85b5cc95c0575daae04d3885853311976c
Hall of Souls <demonsaint.official@gmail.com>
2025-06-20 22:15:54 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,3 +1,4 @@
-# A simple function
-def get_welcome_message(name):
-    return "Hello " + name
+# A better, more modern function
+def get_welcome_message(name: str) -> str:
+    """Greets the user with a formatted string."""
+    return f"Hello, {name}!"
