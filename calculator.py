f10294f45aa2a16c11e7909b2dfb7243ca7c65f1
Hall of Souls <demonsaint.official@gmail.com>
2025-06-21 14:49:46 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,7 @@
-# calculator.py (Version 1)
-
-def add_numbers(a, b):
-    """This function adds two numbers together."""
-    return a + b
-
-result = add_numbers(5, 10)
-print(f"The result is: {result}")
+def get_even_numbers(max_number):
+    """Returns a list of even numbers up to max_number."""
+    even_numbers = []
+    for i in range(max_number + 1):
+        if i % 2 == 0:
+            even_numbers.append(i)
+    return even_numbers
