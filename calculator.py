df55823019a9419cffb74ca1bf1e5924fa027272
shivadeepak <shivadeepak.dev@gmail.com>
2025-06-21 10:58:24 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# calculator.py (Version 1)
+
+def add_numbers(a, b):
+    """This function adds two numbers together."""
+    return a + b
+
+result = add_numbers(5, 10)
+print(f"The result is: {result}")
